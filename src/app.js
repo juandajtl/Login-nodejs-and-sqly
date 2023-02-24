279cf77c6a539a3b2134f89067fdc6d01a1a1b8f
Juan Manuel <justin4949ch@gmail.com>
2023-02-24 15:23:08 -0500

    inicio de base de datos

--- src/app.js
+++ src/app.js
@@ -8,22 +8,38 @@ const bodyParser = require('body-parser');
 const app = express();
 app.set('port', 4000);
 
-app.set('vews', __dirname + '/views')
+app.set('views', __dirname + '/views')
 app.engine('.hbs', engine({
     extname: '.hbs',
 }));
 app.set('view engine', 'hbs');
+
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
 
+app.use(myconnection(mysql, {
+    host: 'localhost',
+    user:'root',
+    password:'',
+    port:'3006',
+    database:'nodelogin'
+}));
+
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}))
 
 app.listen(app.get('port'), () => {
     console.log('Listening on port ', app.get('port'));
 });
 
-app.get('/',(req, res) => {
+
+
+app.get('/', (req, res) => {
     res.render('home');
 });
